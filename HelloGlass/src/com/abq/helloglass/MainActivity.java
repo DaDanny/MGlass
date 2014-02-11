0dbd3b3ca437fa6071228578d15dd4562918db4b
Vijay Ganesh <vbganesh@Vijays-MacBook-Pro.local>
2014-02-10 21:06:48 -0500

    added on key up event

--- HelloGlass/src/com/abq/helloglass/MainActivity.java
+++ HelloGlass/src/com/abq/helloglass/MainActivity.java
@@ -1,6 +1,7 @@
 package com.abq.helloglass;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.app.Activity;
 
 import com.google.android.glass.app.Card;
@@ -9,15 +10,38 @@ import com.google.android.glass.timeline.TimelineManager;
 
 public class MainActivity extends Activity {
 
+	Card helloWorldCard;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		Card helloWorldCard = new Card(this);
-		helloWorldCard.setText("Hello World");
+		helloWorldCard = new Card(this);
+		helloWorldCard.setText("ugli basement");
 		
 		setContentView(helloWorldCard.toView());
 		
-		
 	}
+	@Override
+	public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_DPAD_CENTER) {
+            // user tapped touchpad, do something
+        	helloWorldCard.setText("Vijay R");
+        	setContentView(helloWorldCard.toView());
+            return true;
+        }
+        helloWorldCard.setText("Not Key Up");
+        setContentView(helloWorldCard.toView());
+        return false;
+    }
+	
+	public boolean onKeyUp(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_DPAD_CENTER) {
+            // user tapped touchpad, do something
+        	helloWorldCard.setText("Danny Francken");
+        	setContentView(helloWorldCard.toView());
+            return true;
+        }
+        return false;
+    }
 }
+
